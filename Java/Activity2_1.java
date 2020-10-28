a6da95109e46429c82c63c839134c947c74f24a8
ranjitha1993 <73516493+ranjitha1993@users.noreply.github.com>
2020-10-28 18:53:53 +0530

    Add files via upload

---
+++ Java/Activity2_1.java
@@ -0,0 +1,23 @@
+
+public class Activity2_1 
+{
+	public static void main(String []args) {
+        String title = "Hover Car Racer";
+        Book newNovel = new MyBook();
+        newNovel.setTitle(title);
+        System.out.println("The title is: " + newNovel.getTitle());
+    }
+}
+abstract class Book {
+    String title;
+    abstract void setTitle(String s);
+    String getTitle() {
+        return title;
+    }
+}
+
+class MyBook extends Book {
+    public void setTitle(String s) {
+        title = s;
+    }
+}
