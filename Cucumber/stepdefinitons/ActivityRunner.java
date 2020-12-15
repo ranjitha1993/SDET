410ac1fb61b84b47fdf2ab59de0451e8d7c649af
ranjitha1993 <73516493+ranjitha1993@users.noreply.github.com>
2020-12-15 12:31:10 +0530

    Add files via upload

---
+++ Cucumber/stepdefinitons/ActivityRunner.java
@@ -0,0 +1,22 @@
+package stepdefinitons;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+ 
+@RunWith(Cucumber.class)
+@CucumberOptions(
+    features = "src/test/resources/features",
+    glue = {"stepDefinitions"},
+//  tags ="@activity1_5",
+ plugin = {"html:src/test/java/Reports/test-report.html"},
+//    plugin = {"json:src/test/java/Reports/json-report.json"},
+    monochrome = true
+)
+ 
+
+public class ActivityRunner {
+
+}
